aaff2ca2fb9ef8e8d5069d6eb0b9d59fec32ab7d
CORP\pamahto <parashnath-kumar.mahto@capgemini.com>
2022-04-09 08:33:34 +0530

    Filter and Sorting implementation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
+import Filter from "./components/Filter";
 import Products from "./components/Products";
 import data from "./data.json";
-
 class App extends React.Component {
   constructor() {
     super();
@@ -11,15 +11,63 @@ class App extends React.Component {
       sort: "",
     };
   }
+
+  sortProducts = (event) => {
+    console.log(event.target.value);
+    this.setState({
+      sort: event.target.value,
+      products: this.state.products
+        .slice()
+        .sort((a, b) =>
+          event.target.value === "lowest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : event.target.value === "highest"
+            ? a.price < b.price
+              ? 1
+              : -1
+            : a.price > b.price
+            ? 1
+            : -1
+        ),
+    });
+  };
+
+  filterProducts = (event) => {
+    console.log(event.target.value);
+    if (event.target.value === "ALL") {
+      this.setState({
+        size: event.target.value,
+        products: data.products,
+      });
+    } else {
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(event.target.value) >= 0
+        ),
+      });
+    }
+  };
+
   render() {
     return (
       <div className="grid-container">
         <header className="App-header">
-          <a href="/">Fashion Shopify</a>
+          {/* <img src={process.env.PUBLIC_URL + "/images/logo.jpg"}></img> */}
+          <a href="/">Shopify</a>
         </header>
         <main>
           <div className="content">
             <div className="main">
+              <Filter
+                count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+                filterProducts={this.filterProducts}
+                sortProducts={this.sortProducts}
+              ></Filter>
               <Products products={this.state.products}></Products>
             </div>
             <div className="sidebar">Cart Items</div>
